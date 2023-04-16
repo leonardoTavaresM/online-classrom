b352798d3a96aeae367c2cfa515ebda976b0d9d3
Leonardo <leomaltadesa0712@gmail.com>
2023-04-16 15:07:28 -0300

    feat: starting project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { NextPage } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -122,3 +123,5 @@ export default function Home() {
     </main>
   )
 }
+
+export default Home
